df6b1e30a7eb3cf5e624419f25022c25f57a6ecc
Emma Lu Eikemo <emmalei@stud.ntnu.no>
2022-03-04 17:53:12 +0100

    Got rid of comments related to semafores and threads

--- spill/main.c
+++ spill/main.c
@@ -4,19 +4,14 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-//#include <semaphore.h>
-
 
 #include "snake.c"
 #include "board.c"
-
-//sem_t semaphore_move;
 
 int main() {
     int count_food = 0;
     int do_movement = 0;
     init_board();
-    //char test[50];
  
     //CODE//
     // Snake starts in the middle and is 3 squares big
@@ -26,21 +21,7 @@ int main() {
 
     place_random_food(&count_food);
 
-    /* sem_init(&semaphore_move, 0, 1); // Initialize the semaphore
-    int semaphore_value;
-    int pid = fork(); //Make a process to move snake every second
-    if (pid == 0){
-        while(snake.isAlive)
-        {   
-            //print_board();
-            // sleep(1);
-            //sem_post(&semaphore_move); 
-            //sem_getvalue(&semaphore_move,&semaphore_value);
-            //printf("Inside thread: %d\n", semaphore_value);
-            //move(&snake, &snake.head, snake.direction_x, snake.direction_y, &count_food);
-        }
-        exit(0);
-    } */
+    
     
     while (snake.isAlive){
         int keypress = getchar();
@@ -66,19 +47,16 @@ int main() {
             place_random_food(&count_food);
         }
 
-        //printf("sem value: %d\n",sem_getvalue(&semaphore_move,&semaphore_value));
+        
 
-        if(do_movement){ //Should be set to 1 every second, semaphore_value==1
+        if(do_movement){ //Should be set to 1 every second
             printf("Inside if\n");
             move(&snake, &snake.head, snake.direction_x, snake.direction_y, &count_food);
             print_board();
             do_movement = 0;
-            //sem_wait(&semaphore_move);
+            
         }
     }
 
-    //int waitstatus; //For degbug of status of the child
-    //waitpid(pid, &waitstatus, 0); //Only one child, so need only wait for one child process to finish
-    //sem_destroy(&semaphore_move); // Free the memory of the semaphore
     return(0);
 }
