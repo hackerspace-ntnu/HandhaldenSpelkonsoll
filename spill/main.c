aa48b0efb4ecdaa1fb8f51915d84c46f4fd931f6
Emma Lu Eikemo <emmalei@stud.ntnu.no>
2022-03-23 17:44:44 +0100

    Made simple multiplayer example

--- spill/main.c
+++ spill/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "snake.c"
 #include "board.c"
@@ -12,29 +13,30 @@ int main() {
     int count_food = 0;
     int do_movement = 1;
     init_board();
-    
+   
     struct Snake snake1;
     struct Snake snake2;
     struct Snake snake3;
     struct Snake snake4;
+ 
 
-    int init_coordinates1[][2] = {{(BOARD_WIDTH/2)+2, (BOARD_HEIGHT/2)}, {(BOARD_WIDTH/2)+1, (BOARD_HEIGHT/2)}, {BOARD_WIDTH/2, BOARD_HEIGHT/2}}; 
-    int init_coordinates2[][2] = {{BOARD_WIDTH-3, BOARD_HEIGHT}, {BOARD_WIDTH-2, BOARD_HEIGHT}, {BOARD_WIDTH-1, BOARD_HEIGHT}}; 
-    int init_coordinates3[][2] = {{2, BOARD_HEIGHT}, {1, BOARD_HEIGHT}, {0, BOARD_HEIGHT}}; 
-    int init_coordinates4[][2] = {{BOARD_WIDTH-3, 0}, {BOARD_WIDTH-2, 0}, {BOARD_WIDTH-1, 0}}; 
-
+    int init_coordinates1[][2] = {{0, 0}, {1, 0}, {2, 0}}; 
+    int init_coordinates2[][2] = {{BOARD_WIDTH-0, BOARD_HEIGHT-1}, {BOARD_WIDTH-2, BOARD_HEIGHT-1}, {BOARD_WIDTH-3, BOARD_HEIGHT-1}}; 
+    int init_coordinates3[][2] = {{0, BOARD_HEIGHT-1}, {1, BOARD_HEIGHT-1}, {2, BOARD_HEIGHT-1}}; 
+    int init_coordinates4[][2] = {{BOARD_WIDTH-1, 0}, {BOARD_WIDTH-2, 0}, {BOARD_WIDTH-3, 0}}; 
+    
     //CODE//
     // 1 player: snake1 starts in the middle and is 3 squares big
     // 1 < player: snake1 starts in the upper left corner and is 3 squares big
     switch (PLAYER_NUMBER){
     case 1:
         // Middle of the board (There must be a better way to implement this, but I'm sleepy)
-        init_coordinates1[0][0] = 2; 
-        init_coordinates1[1][0] = 1; 
-        init_coordinates1[2][0] = 0; 
-        init_coordinates1[0][1] = 0; 
-        init_coordinates1[1][1] = 0; 
-        init_coordinates1[2][1] = 0; 
+        init_coordinates1[0][0] = (BOARD_WIDTH/2); 
+        init_coordinates1[1][0] = (BOARD_WIDTH/2)+1; 
+        init_coordinates1[2][0] = (BOARD_WIDTH/2)+2; 
+        init_coordinates1[0][1] = BOARD_HEIGHT/2; 
+        init_coordinates1[1][1] = BOARD_HEIGHT/2; 
+        init_coordinates1[2][1] = BOARD_HEIGHT/2; 
 
         snake1 = create_snake(3, init_coordinates1);
         add_snake_to_board(snake1.head);
@@ -70,12 +72,13 @@ int main() {
         snake1 = create_snake(3, init_coordinates1);
         snake2 = create_snake(3, init_coordinates2);
         snake3 = create_snake(3, init_coordinates3);
-        snake3 = create_snake(3, init_coordinates4);
+        snake4 = create_snake(3, init_coordinates4);
+       
         add_snake_to_board(snake1.head);
         add_snake_to_board(snake2.head);
         add_snake_to_board(snake3.head);
         add_snake_to_board(snake4.head);
-
+       
         //initial food
         place_random_food(&count_food);
         place_random_food(&count_food);
@@ -85,7 +88,7 @@ int main() {
         return 1; //Error, no more players than 4, and no negative players
         break;
     }
-    
+   
     while (snake1.isAlive){
         int keypress = getchar();
         fflush(stdin);
@@ -106,9 +109,17 @@ int main() {
                 break;
         }
 
+        //Just to test multiple snake movement
+        set_direction(&snake2, (rand() % 4));
+        set_direction(&snake3, (rand() % 4));
+        set_direction(&snake4, (rand() % 4));
+
         if(do_movement){ //Should be set to 1 every second
             printf("Inside if\n");
             move(&snake1, &snake1.head, snake1.direction_x, snake1.direction_y, &count_food);
+            move(&snake2, &snake2.head, snake2.direction_x, snake2.direction_y, &count_food);
+            move(&snake3, &snake3.head, snake3.direction_x, snake3.direction_y, &count_food);
+            move(&snake4, &snake4.head, snake4.direction_x, snake4.direction_y, &count_food);
             print_board();
             //do_movement = 0;
             
