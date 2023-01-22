a2ed73548a938731d28fe6d1ee2a6ac9d40fc8be
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2023-01-22 14:16:46 +0100

    deleted obsolete files and moved the multiplayer function to main

--- main/app_main.c
+++ main/app_main.c
@@ -2,7 +2,18 @@
 #include "snake.h"
 #include "constants.h"
 
-
+// void get_multiplayer_struct(struct multiplayer_info *multi_info, struct multiplayer_info **multi_info_buffer, int *stack_pointer){
+//     if(multi_info->id > 3){ // Our snakes have ids in the 0-4 range
+//         printf("Unknown snake id: %d", multi_info->id);
+//     }
+//     if (*stack_pointer < MULTIPLAYER_BUFFER_SIZE){
+//         multi_info_buffer[*stack_pointer] = multi_info;
+//         *stack_pointer++;
+//     }    
+//     else{
+//         printf("No more space on stack, struct dropped\n");
+//     }
+// }
 
 // void draw_board();
 // void gfx_init();
@@ -77,6 +88,8 @@ void app_main(void) {
       snake1.isAlive = false;
       snake2.isAlive = false;
     }
+
+
 }
 
 
