a1b2149da610cbff016586713472c3a1bcf5a30f
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2023-01-26 10:59:00 +0100

    modified espnow example

--- main/espnow.h
+++ main/espnow.h
@@ -6,9 +6,27 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <assert.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+#include "nvs_flash.h"
+#include "esp_random.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_mac.h"
+#include "esp_now.h"
+#include "esp_crc.h"
+#include "esp_err.h"
 
-#ifndef ESPNOW_EXAMPLE_H
-#define ESPNOW_EXAMPLE_H
+
+#ifndef ESPNOW_H
+#define ESPNOW_H
 
 /* ESPNOW can work in both station and softap mode. It is configured in menuconfig. */
 #if CONFIG_ESPNOW_WIFI_MODE_STATION
@@ -19,44 +37,48 @@
 #define ESPNOW_WIFI_IF   ESP_IF_WIFI_AP
 #endif
 
-#define ESPNOW_QUEUE_SIZE           6
+#define ESPNOW_QUEUE_SIZE 6
 
-#define IS_BROADCAST_ADDR(addr) (memcmp(addr, s_example_broadcast_mac, ESP_NOW_ETH_ALEN) == 0)
+#define IS_BROADCAST_ADDR(addr) (memcmp(addr, s_broadcast_mac, ESP_NOW_ETH_ALEN) == 0)
+
+// #undef ESP_ERROR_CHECK
+// #define ESP_ERROR_CHECK(x)   do { esp_err_t rc = (x); if (rc != ESP_OK) { ESP_LOGE("err", "esp_err_t = %d", rc); assert(0 && #x);} } while(0)
+
 
 typedef enum {
-    EXAMPLE_ESPNOW_SEND_CB,
-    EXAMPLE_ESPNOW_RECV_CB,
-} example_espnow_event_id_t;
+    ESPNOW_SEND_CB,
+    ESPNOW_RECV_CB,
+} espnow_event_id_t;
 
 typedef struct {
     uint8_t mac_addr[ESP_NOW_ETH_ALEN];
     esp_now_send_status_t status;
-} example_espnow_event_send_cb_t;
+} espnow_event_send_cb_t;
 
 typedef struct {
     uint8_t mac_addr[ESP_NOW_ETH_ALEN];
     uint8_t *data;
     int data_len;
-} example_espnow_event_recv_cb_t;
+} espnow_event_recv_cb_t;
 
 typedef union {
-    example_espnow_event_send_cb_t send_cb;
-    example_espnow_event_recv_cb_t recv_cb;
-} example_espnow_event_info_t;
+    espnow_event_send_cb_t send_cb;
+    espnow_event_recv_cb_t recv_cb;
+} espnow_event_info_t;
 
 /* When ESPNOW sending or receiving callback function is called, post event to ESPNOW task. */
 typedef struct {
-    example_espnow_event_id_t id;
-    example_espnow_event_info_t info;
-} example_espnow_event_t;
+    espnow_event_id_t id;
+    espnow_event_info_t info;
+} espnow_event_t;
 
 enum {
-    EXAMPLE_ESPNOW_DATA_BROADCAST,
-    EXAMPLE_ESPNOW_DATA_UNICAST,
-    EXAMPLE_ESPNOW_DATA_MAX,
+    ESPNOW_DATA_BROADCAST,
+    ESPNOW_DATA_UNICAST,
+    ESPNOW_DATA_MAX,
 };
 
-/* User defined field of ESPNOW data in this example. */
+/* User defined field of ESPNOW data. */
 typedef struct {
     uint8_t type;                         //Broadcast or unicast ESPNOW data.
     uint8_t state;                        //Indicate that if has received broadcast ESPNOW data or not.
@@ -64,7 +86,7 @@ typedef struct {
     uint16_t crc;                         //CRC16 value of ESPNOW data.
     uint32_t magic;                       //Magic number which is used to determine which device to send unicast ESPNOW data.
     uint8_t payload[0];                   //Real payload of ESPNOW data.
-} __attribute__((packed)) example_espnow_data_t;
+} __attribute__((packed)) espnow_data_t;
 
 /* Parameters of sending ESPNOW data. */
 typedef struct {
@@ -77,6 +99,17 @@ typedef struct {
     int len;                              //Length of ESPNOW data to be sent, unit: byte.
     uint8_t *buffer;                      //Buffer pointing to ESPNOW data.
     uint8_t dest_mac[ESP_NOW_ETH_ALEN];   //MAC address of destination device.
-} example_espnow_send_param_t;
+} espnow_send_param_t;
+
+void espnow_deinit(espnow_send_param_t *send_param);
+void wifi_init(void);
+ void espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_t status);
+ void espnow_recv_cb(const uint8_t *mac_addr, const uint8_t *data, int len);
+int espnow_data_parse(uint8_t *data, uint16_t data_len, uint8_t *state, uint16_t *seq, int *magic);
+void espnow_data_prepare(espnow_send_param_t *send_param);
+ void espnow_task(void *pvParameter);
+ esp_err_t espnow_init(void);
+ void espnow_deinit(espnow_send_param_t *send_param);
+
 
 #endif
