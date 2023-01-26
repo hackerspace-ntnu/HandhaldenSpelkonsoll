58ce095e3ecef4c34e0b62eb5d9c367f79427924
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2023-01-26 12:27:24 +0100

    removed some comments

--- main/comms.c
+++ main/comms.c
@@ -1,12 +1,3 @@
-/* ESPNOW Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 /*
    This example shows how to use ESPNOW.
    Prepare two device, one for sending ESPNOW data and another for receiving
