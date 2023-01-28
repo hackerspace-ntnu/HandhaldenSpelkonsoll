493e7f7b4b7a4594480b193a86f893581b31a11a
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2023-01-28 14:04:38 +0100

    created new files for our multiplayer stuff

---
+++ main/multiplayer.h
@@ -0,0 +1,121 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <assert.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+#include "nvs_flash.h"
+#include "esp_random.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_mac.h"
+#include "esp_now.h"
+#include "esp_crc.h"
+#include "esp_err.h"
+
+
+#ifndef MULTIPLAYER_H
+#define MULTIPLAYER_H
+
+/* ESPNOW can work in both station and softap mode. It is configured in menuconfig. */
+#if CONFIG_ESPNOW_WIFI_MODE_STATION
+#define ESPNOW_WIFI_MODE WIFI_MODE_STA
+#define ESPNOW_WIFI_IF   ESP_IF_WIFI_STA
+#else
+#define ESPNOW_WIFI_MODE WIFI_MODE_AP
+#define ESPNOW_WIFI_IF   ESP_IF_WIFI_AP
+#endif
+
+#define ESPNOW_QUEUE_SIZE 6
+#define ESPNOW_MAXDELAY 512
+
+#define IS_BROADCAST_ADDR(addr) (memcmp(addr, s_broadcast_mac, ESP_NOW_ETH_ALEN) == 0)
+
+typedef enum {
+    ESPNOW_SEND_CB,
+    ESPNOW_RECV_CB,
+} espnow_event_id_t;
+
+typedef struct {
+    uint8_t mac_addr[ESP_NOW_ETH_ALEN];
+    esp_now_send_status_t status;
+} espnow_event_send_cb_t;
+
+typedef struct {
+    uint8_t mac_addr[ESP_NOW_ETH_ALEN];
+    uint8_t *data;
+    int data_len;
+} espnow_event_recv_cb_t;
+
+typedef union {
+    espnow_event_send_cb_t send_cb;
+    espnow_event_recv_cb_t recv_cb;
+} espnow_event_info_t;
+
+/* When ESPNOW sending or receiving callback function is called, post event to ESPNOW task. */
+typedef struct {
+    espnow_event_id_t id;
+    espnow_event_info_t info;
+} espnow_event_t;
+
+enum {
+    ESPNOW_DATA_BROADCAST,
+    ESPNOW_DATA_UNICAST,
+    ESPNOW_DATA_MAX,
+};
+
+
+typedef struct {
+    short int id; // To know which snake to update
+    int direction; // UP, DOWN, LEFT OR RIGHT
+    int new_food; // 0 = no new food, 1 = new food (check for its coordinates)
+    int food_coord_x; // When new food is placed, use this to signal where
+    int food_coord_y;// When new food is placed, use this to signal where
+    uint8_t type;                         //Broadcast or unicast ESPNOW data.
+    uint8_t state;                        //Indicate that if has received broadcast ESPNOW data or not.
+    uint16_t seq_num;                     //Sequence number of ESPNOW data.
+    uint16_t crc;                         //CRC16 value of ESPNOW data.
+    uint32_t magic;                       //Magic number which is used to determine which device to send unicast ESPNOW data.
+} multiplayer_data_t;
+
+
+/* User defined field of ESPNOW data. */
+typedef struct {
+    uint8_t type;                         //Broadcast or unicast ESPNOW data.
+    uint8_t state;                        //Indicate that if has received broadcast ESPNOW data or not.
+    uint16_t seq_num;                     //Sequence number of ESPNOW data.
+    uint16_t crc;                         //CRC16 value of ESPNOW data.
+    uint32_t magic;                       //Magic number which is used to determine which device to send unicast ESPNOW data.
+    uint8_t payload[0];                   //Real payload of ESPNOW data.
+} __attribute__((packed)) espnow_data_t;
+
+
+
+/* Parameters of sending ESPNOW data. */
+typedef struct {
+    bool unicast;                         //Send unicast ESPNOW data.
+    bool broadcast;                       //Send broadcast ESPNOW data.
+    uint8_t state;                        //Indicate that if has received broadcast ESPNOW data or not.
+    uint32_t magic;                       //Magic number which is used to determine which device to send unicast ESPNOW data.
+    uint16_t count;                       //Total count of unicast ESPNOW data to be sent.
+    uint16_t delay;                       //Delay between sending two ESPNOW data, unit: ms.
+    int len;                              //Length of ESPNOW data to be sent, unit: byte.
+    uint8_t *buffer;                      //Buffer pointing to ESPNOW data.
+    uint8_t dest_mac[ESP_NOW_ETH_ALEN];   //MAC address of destination device.
+} espnow_send_param_t;
+
+void espnow_deinit(espnow_send_param_t *send_param);
+void wifi_init(void);
+void espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_t status);
+void espnow_recv_cb(const uint8_t *mac_addr, const uint8_t *data, int len);
+int espnow_data_parse(uint8_t *data, uint16_t data_len, uint8_t *state, uint16_t *seq, int *magic);
+void espnow_data_prepare(espnow_send_param_t *send_param);
+void espnow_task(void *pvParameter);
+esp_err_t espnow_init(void);
+void espnow_deinit(espnow_send_param_t *send_param);
+void print_multiplayer_info(uint8_t* info);
+
+#endif
