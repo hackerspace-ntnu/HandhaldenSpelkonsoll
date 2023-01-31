d595a254057c75678369603aa8b34c965515b9be
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2023-01-31 11:57:45 +0100

    adede lvgl and drivers

--- main/app_main.c
+++ main/app_main.c
@@ -3,6 +3,8 @@
 #include "constants.h"
 #include "button.h"
 
+#include "../components/lvgl/lvgl.h"
+
 // void get_multiplayer_struct(struct multiplayer_info *multi_info, struct multiplayer_info **multi_info_buffer, int *stack_pointer){
 //     if(multi_info->id > 3){ // Our snakes have ids in the 0-4 range
 //         printf("Unknown snake id: %d", multi_info->id);
@@ -21,74 +23,82 @@
 
 void app_main(void) {
 
-    
-    int tick = 0;
-    int count_food = 0;
-    int do_movement = 1;
+     lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x003a57), LV_PART_MAIN);
 
-    // struct multiplayer_info **multi_info_buffer = NULL;
+    /*Create a white label, set its text and align it to the center*/
+    lv_obj_t * label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "Hello world");
+    lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-    // int *multi_info_buffer_stack_pointer = (int*)malloc(sizeof(int));
-    int snake_id_counter = 0;
+        
+    //     int tick = 0;
+    //     int count_food = 0;
+    //     int do_movement = 1;
 
-    struct Snake snake1;
-    struct Snake snake2;
+    //     // struct multiplayer_info **multi_info_buffer = NULL;
 
-    int init_coordinates1[][2] = {{0, 0}, {1, 0}, {2, 0}}; 
-    int init_coordinates2[][2] = {{BOARD_WIDTH-0, BOARD_HEIGHT-1}, {BOARD_WIDTH-2, BOARD_HEIGHT-1}, {BOARD_WIDTH-3, BOARD_HEIGHT-1}}; 
-//   gfx_init();
-  // multi_info_buffer = (struct multiplayer_info**)malloc(sizeof(struct multiplayer_info)*MULTIPLAYER_BUFFER_SIZE);
-  // *multi_info_buffer_stack_pointer = 0;
-  init_board();
+    //     // int *multi_info_buffer_stack_pointer = (int*)malloc(sizeof(int));
+    //     int snake_id_counter = 0;
 
-  switch (PLAYER_NUMBER){
-    case 1:
-        // Middle of the board (There must be a better way to implement this, but I'm sleepy)
-        init_coordinates1[0][0] = (BOARD_WIDTH/2); 
-        init_coordinates1[1][0] = (BOARD_WIDTH/2)+1; 
-        init_coordinates1[2][0] = (BOARD_WIDTH/2)+2; 
-        init_coordinates1[0][1] = BOARD_HEIGHT/2; 
-        init_coordinates1[1][1] = BOARD_HEIGHT/2; 
-        init_coordinates1[2][1] = BOARD_HEIGHT/2; 
+    //     struct Snake snake1;
+    //     struct Snake snake2;
 
-        snake1 = create_snake(3, init_coordinates1, &snake_id_counter);
-        add_snake_to_board(snake1.head);
+    //     int init_coordinates1[][2] = {{0, 0}, {1, 0}, {2, 0}}; 
+    //     int init_coordinates2[][2] = {{BOARD_WIDTH-0, BOARD_HEIGHT-1}, {BOARD_WIDTH-2, BOARD_HEIGHT-1}, {BOARD_WIDTH-3, BOARD_HEIGHT-1}}; 
+    // //   gfx_init();
+    // // multi_info_buffer = (struct multiplayer_info**)malloc(sizeof(struct multiplayer_info)*MULTIPLAYER_BUFFER_SIZE);
+    // // *multi_info_buffer_stack_pointer = 0;
+    // init_board();
 
-        //initial food
-        place_random_food(&count_food); 
-        break;
+    // switch (PLAYER_NUMBER){
+    //     case 1:
+    //         // Middle of the board (There must be a better way to implement this, but I'm sleepy)
+    //         init_coordinates1[0][0] = (BOARD_WIDTH/2); 
+    //         init_coordinates1[1][0] = (BOARD_WIDTH/2)+1; 
+    //         init_coordinates1[2][0] = (BOARD_WIDTH/2)+2; 
+    //         init_coordinates1[0][1] = BOARD_HEIGHT/2; 
+    //         init_coordinates1[1][1] = BOARD_HEIGHT/2; 
+    //         init_coordinates1[2][1] = BOARD_HEIGHT/2; 
 
-    case 2:
-        snake1 = create_snake(3, init_coordinates1, &snake_id_counter);
-        snake2 = create_snake(3, init_coordinates2, &snake_id_counter);
-        add_snake_to_board(snake1.head);
-        add_snake_to_board(snake2.head);
+    //         snake1 = create_snake(3, init_coordinates1, &snake_id_counter);
+    //         add_snake_to_board(snake1.head);
 
-        //initial food
-        place_random_food(&count_food);
-        break;
-    
-    default:
-        break;
-    }
-    while (snake1.isAlive) {
+    //         //initial food
+    //         place_random_food(&count_food); 
+    //         break;
 
-        //Just to test multiple snake movement
-        set_direction(&snake2, (rand() % 4));
-        printf("Snake1 x: %d, y: %d\n", snake1.direction_x, snake1.direction_y);
-        printf("Snake2 x: %d, y: %d\n", snake2.direction_x, snake2.direction_y);  
+    //     case 2:
+    //         snake1 = create_snake(3, init_coordinates1, &snake_id_counter);
+    //         snake2 = create_snake(3, init_coordinates2, &snake_id_counter);
+    //         add_snake_to_board(snake1.head);
+    //         add_snake_to_board(snake2.head);
 
-        if(do_movement){ //Should be set to 1 every second
-            move(&snake1, &snake1.head, snake1.direction_x, snake1.direction_y, &count_food);
-            move(&snake2, &snake2.head, snake2.direction_x, snake2.direction_y, &count_food);
-        }
-        print_board();  
-        tick++;          
-    }
-    if (tick > 20){
-      snake1.isAlive = false;
-      snake2.isAlive = false;
-    }
+    //         //initial food
+    //         place_random_food(&count_food);
+    //         break;
+        
+    //     default:
+    //         break;
+    //     }
+    //     while (snake1.isAlive) {
+
+    //         //Just to test multiple snake movement
+    //         set_direction(&snake2, (rand() % 4));
+    //         printf("Snake1 x: %d, y: %d\n", snake1.direction_x, snake1.direction_y);
+    //         printf("Snake2 x: %d, y: %d\n", snake2.direction_x, snake2.direction_y);  
+
+    //         if(do_movement){ //Should be set to 1 every second
+    //             move(&snake1, &snake1.head, snake1.direction_x, snake1.direction_y, &count_food);
+    //             move(&snake2, &snake2.head, snake2.direction_x, snake2.direction_y, &count_food);
+    //         }
+    //         print_board();  
+    //         tick++;          
+    //     }
+    //     if (tick > 20){
+    //     snake1.isAlive = false;
+    //     snake2.isAlive = false;
+    //     }
 
 
 }
